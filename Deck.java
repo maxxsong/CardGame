c0fdc3fd754988fde22195dd4c97ba74af17e5ed
Mymomisawesome <richard.hwang1@gmail.com>
2016-03-10 13:41:40 -0800

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -28,7 +28,31 @@ public class Deck {
 		return dk.remove(dk.size()-1);
 	}
 	
+	public void deal(ArrayList<Player>players){
+		for (int j=0;j<dk.size();j++){
+			for (int k=0;k<players.size();k++){
+				//Need to work on
+			}
+		}
+	}
+	
 	public int cardsLeft(){
 		return dk.size();
+	}
+	
+	public int cardsDealt(){
+		return 52-dk.size();
+	}
+	
+	public boolean hasNext(){
+		return dk.size()==0;
+	}
+	
+	void replenish(){
+		for(int i = 0; i< 4; i++){
+			for(int j = 1; j <= 13; j++){
+				dk.add(new Card(j, Suit.values()[i]));
+			}
+		}
 	}
 }
