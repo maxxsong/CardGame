fd37738b3f3ea0ed85ec5018f81da7db6c200719
maxxsong <maxx1999syp@163.com>
2016-03-10 17:04:38 -0800

    Player updated

--- Player.java
+++ Player.java
@@ -1,20 +1,39 @@
-import java.security.PublicKey;
 import java.util.ArrayList;
 
 public class Player {
-	private ArrayList<Card>hand = new ArrayList<Card>();
-	private String name;
-	public void add(Card c){
-		hand.add(c);
-	}
-	public Card next(){
-		return hand.remove(hand.size()-1);
-	}
-	public boolean hasNext(){
-		return (hand.size()==0);
-	}
-	public Player(String name){
-		this.name = name;
-	}
-	//lol will this create a merge conflict?
+    private ArrayList<Card>hand = new ArrayList<Card>();
+    private String name;
+    //add
+    public void add(Card c){
+        hand.add(c);
+    }
+    public void add(ArrayList<Card>al){
+        hand.addAll(al);
+    }
+    //return
+    public Card nextCard(){
+        return hand.get(hand.size()-1);
+    }
+    public Card playNext(){
+        if (hand.size()==0) {
+            System.exit(1);
+        }
+        return hand.remove(hand.size()-1);
+    }
+    //can continue?
+    public boolean hasNext(){
+        return (hand.size()>0);
+    }
+    public int numLeft(){
+        return hand.size();
+    }
+    //get info
+    public String getName(){
+        return name;
+    }
+
+    //Constructor
+    public Player(String name){
+        this.name = name;
+    }
 }
