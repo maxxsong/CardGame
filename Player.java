31816772ec332987ebe519e8ca1fbc1a892a1e83
maxxsong <maxx1999syp@163.com>
2016-03-15 09:07:45 -0700

    Statistics added

--- Player.java
+++ Player.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class Player {
     private ArrayList<Card>hand = new ArrayList<Card>();
     private String name;
+    private int wins;
+    private int losses;
     //add
     public void add(Card c){
         hand.add(c);
@@ -32,8 +34,21 @@ public class Player {
         return name;
     }
 
+    //stats:
+    public void isWinner(){
+        wins++;
+    }
+    public void isLoser(){
+        losses++;
+    }
+
+    public String toString(){
+        return "Player " + name + " won " + wins + " rounds and lost " + losses + " rounds.";
+    }
     //Constructor
     public Player(String name){
         this.name = name;
+        wins = 0;
+        losses = 0;
     }
 }
